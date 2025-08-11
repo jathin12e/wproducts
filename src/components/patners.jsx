cddc7a7eca81cdc63c3d71c365a430c0175c1ee4
jathin12e <126076303+jathin12e@users.noreply.github.com>
2025-08-11 14:03:13 +0530

    changed

--- src/components/patners.jsx
+++ src/components/patners.jsx
@@ -4,10 +4,10 @@ import './patners.css';
 const partners = [
   '/images/Screenshot 2025-07-14 155808-Photoroom.png',
   '/images/Screenshot_2025-08-08_131502-removebg-preview.png',
-  '/images/Screenshot 2025-08-05 175913-Photoroom.png',
+  
   '/images/Screenshot 2025-08-05 175754-Photoroom.png',
   '/images/Screenshot 2025-08-05 180830-Photoroom.png',
-  '/images/Screenshot 2025-08-06 102223-Photoroom.png',
+  
   '/images/Screenshot 2025-08-06 102444-Photoroom.png',
   
 ];
